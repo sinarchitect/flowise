2a30ca493da69a9137e796ba3e9faff0244203f6
sinarchitect.89@gmail.com <sinarchitect.89@gmail.com>
2024-08-21 01:36:21 +0330

    change prediction

--- src/queries/sendMessageQuery.ts
+++ src/queries/sendMessageQuery.ts
@@ -69,7 +69,8 @@ export const updateFeedbackQuery = ({ id, apiHost = 'http://localhost:3000', bod
 export const sendMessageQuery = ({ chatflowid, apiHost = 'http://localhost:3000', body }: MessageRequest) =>
   sendRequest<any>({
     method: 'POST',
-    url: `${apiHost}/api/v1/prediction/${chatflowid}`,
+    // url: `${apiHost}/api/v1/prediction/${chatflowid}`,
+    url: `https://13n5jik04g.execute-api.us-east-2.amazonaws.com/prediction`,
     body,
   });
 
